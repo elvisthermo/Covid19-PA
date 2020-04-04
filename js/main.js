40e43cd3f73fa977af59fda7323fb38ee782c957
elvis thermo <elvis.thermo@gmail.com>
2020-04-03 21:06:00 -0300

    atualizando dados

--- js/main.js
+++ js/main.js
@@ -115,16 +115,9 @@ async function start() {
                     "numero de casos",
                 ],
                 data: count_by_local.map((d) => { return d.QTD }),
-                backgroundColor: [
-                    '#66c2a5',
-                    '#fc8d62',
-                    '#8da0cb',
-                    '#e78ac3',
-                    '#a6d854',
-                    '#ffd92f',
-                    "#e5c494",
-                    "#b3b3b3"
-                ],
+                backgroundColor:
+                    '#e15759'
+                ,
                 borderColor:
                     "#pink"
                 ,
